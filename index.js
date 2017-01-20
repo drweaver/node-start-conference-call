6f14f9148849992e61ce4b92f1a1ab2d8f386abd
Shane Weaver <sgtw_speed@yahoo.com>
2017-01-20 09:54:38 +0000

    added calendar stub - hardcoded

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var pubnub = require('./lib/pubnub.js');
-var eventbus = require('./lib/evenbus.js');
+var eventbus = require('./lib/eventbus.js');
+var calendar = require('./lib/calendar.js');
+const opn = require('opn');
+
 
 pubnub.start();
+
+eventbus.on('startconferencecall', function() {
+    
+    calendar.getClosest(function(conferenceUrl) {
+        console.log("Attempting to open: " + conferenceUrl);
+        opn(conferenceUrl);        
+    });
+
+});
